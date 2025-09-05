cb19a8d4e3bc9662b39ba17ee5e5abad8e950060
frepla <fredrikplanck@gmail.com>
2025-09-05 12:20:50 +0200

    Initial commit

---
+++ src/main/java/com/wigell/secureblogproject/SecureBlogProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wigell.secureblogproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureBlogProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureBlogProjectApplication.class, args);
+    }
+
+}
